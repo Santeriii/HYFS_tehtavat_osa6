48723547844d1e849003bce5c5c24cbc63190603
Santeri Einola <einola.santeri@gmail.com>
2021-04-20 20:11:36 +0300

    tehtävä 6.12 valmis

--- redux-anecdotes/src/components/AnecdoteList.js
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -3,11 +3,13 @@ import { useDispatch, useSelector } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
 import { notificationChange, notificationReset } from '../reducers/notificationReducer'
 import Notification from './Notification'
+import Filter from './Filter'
 
 const AnecdoteList = () => {
     const dispatch = useDispatch()
     const anecdotes = useSelector(state => state.anecdotes)
     const notification = useSelector(state => state.notification)
+    const filter = useSelector(state => state.filter)
 
     const voteFunction = (id) => {
         dispatch(vote(id))
@@ -23,7 +25,9 @@ const AnecdoteList = () => {
             {notification &&
                 <Notification />
             }
+            <Filter />
             {anecdotes
+                .filter(a => a.content.includes(filter.filter))
                 .sort((a, b) => a.votes < b.votes ? 1 : -1)
                 .map(anecdote =>
                 <div key={anecdote.id}>
